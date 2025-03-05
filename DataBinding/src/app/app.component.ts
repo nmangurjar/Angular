9cb4a978d02b6687504349806d4b4adb8b792fa9
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-03-05 15:05:47 +0530

    [Naman] Add. Added Logo

--- DataBinding/src/app/app.component.ts
+++ DataBinding/src/app/app.component.ts
@@ -8,8 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   message: string;
+  logoPath: string;
 
   constructor() {
     this.message = 'Hello from BridgeLabz';
+    this.logoPath = 'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
   }
 }
+
