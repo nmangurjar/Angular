ba6fe0d7fb3e9039126f2f16c4f02b5e03416b03
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-03-05 15:21:30 +0530

    [Naman] Add. Open new Tab on clicking BridgeLabz Logo

--- DataBinding/src/app/app.component.ts
+++ DataBinding/src/app/app.component.ts
@@ -7,12 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-  message: string;
+
   logoPath: string;
+  bridgelabzUrl: string;
 
   constructor() {
-    this.message = 'Hello from BridgeLabz';
+
     this.logoPath = 'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
+    this.bridgelabzUrl = 'https://www.bridgelabz.com';
+  }
+
+  openBridgeLabzSite() {
+    window.open(this.bridgelabzUrl, '_blank');
   }
 }
+
 
