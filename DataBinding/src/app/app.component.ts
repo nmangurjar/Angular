589584f875c744cd90e2ec498de6934ce9612ae5
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-03-05 16:15:01 +0530

    [Naman] Add. In Case Error display the error

--- DataBinding/src/app/app.component.ts
+++ DataBinding/src/app/app.component.ts
@@ -12,8 +12,18 @@ export class AppComponent {
   logoPath: string = 'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
   bridgelabzUrl: string = 'https://www.bridgelabz.com';
   userName: string = '';  // This will store the user input
+  errorMessage: string = '';
 
   openBridgeLabzSite() {
     window.open(this.bridgelabzUrl, '_blank');
+  }
+
+  validateUserName() {
+    const regex = /^[A-Z][a-zA-Z]{2,}$/;
+    if (this.userName.match(regex)) {
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = 'Invalid Name! Name should start with an uppercase letter and have at least 3 letters.';
+    }
   }
 }
