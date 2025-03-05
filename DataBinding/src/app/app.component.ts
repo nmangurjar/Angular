40041df8261454d697000aa817baff13a89a482e
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-03-05 15:55:17 +0530

    [Naman] Add. User Inputted Name

--- DataBinding/src/app/app.component.ts
+++ DataBinding/src/app/app.component.ts
@@ -1,25 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
-
-  logoPath: string;
-  bridgelabzUrl: string;
-
-  constructor() {
-
-    this.logoPath = 'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
-    this.bridgelabzUrl = 'https://www.bridgelabz.com';
-  }
+  logoPath: string = 'https://images.yourstory.com/cs/images/companies/logo-1586419574337.jpg?fm=auto&ar=1%3A1&mode=fill&fill=solid&fill-color=fff&format=auto&w=384&q=75';
+  bridgelabzUrl: string = 'https://www.bridgelabz.com';
+  userName: string = '';  // This will store the user input
 
   openBridgeLabzSite() {
     window.open(this.bridgelabzUrl, '_blank');
   }
 }
-
-
